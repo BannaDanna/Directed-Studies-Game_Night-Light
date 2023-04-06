15a11c6e430c000b926344ed7a282c892aee4e98
JT <JSTing08@gmail.com>
2023-04-06 11:01:48 -1000

    added reactive stamina bar

--- handler/HUD/HUD.java
+++ handler/HUD/HUD.java
@@ -23,7 +23,7 @@ public class HUD {
         HPIconY = (int) (handler.getHeight() - HPIconHeight - (handler.getHeight() * 0.009));
         HPx = HPIconX + (HPIconWidth / 2);
         HPy = HPIconY + (HPIconHeight / 2);
-        staminaX = handler.getWidth() / 2;
+        staminaX = handler.getWidth() / 3;
         staminaY = handler.getHeight() - 56;
     }
 
@@ -35,7 +35,9 @@ public class HUD {
     public void render(Graphics g)
     {
         g.drawImage(Assets.batteries, HPIconX, HPIconY, HPIconWidth, HPIconHeight, null);
+        g.setColor(Color.blue);
+        g.fillRect(staminaX, staminaY, ((handler.getWidth() / 3) / 100) * handler.getWorld().getEntityManager().getPlayer().getStamina() ,handler.getHeight() / 25);
         Text.drawString(g, Integer.toString(handler.getWorld().getEntityManager().getPlayer().getHealth()), HPx, HPy, true, Color.BLACK, Assets.font56);
-        Text.drawString(g, Integer.toString(handler.getWorld().getEntityManager().getPlayer().getStamina()), staminaX, staminaY, true, Color.blue, Assets.font56);
+//        Text.drawString(g, Integer.toString(handler.getWorld().getEntityManager().getPlayer().getStamina()), staminaX, staminaY, true, Color.blue, Assets.font56);
     }
 }
